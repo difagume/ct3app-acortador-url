3d5b8a4990a647d6d0bbc7fcc054fb06330a0789
Diego Fabricio <difagume@gmail.com>
2024-01-21 17:19:23 -0500

    manifest and images

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from 'next/font/google'
 
 import { ThemeProvider } from '@/components/ui/theme-provider'
 import { Toaster } from '@/components/ui/toaster'
+import { Viewport } from 'next'
 
 const inter = Inter({
 	subsets: ['latin'],
@@ -13,7 +14,17 @@ const inter = Inter({
 export const metadata = {
 	title: 'Acortador de URL',
 	description: 'Generador de URLs cortas',
-	icons: [{ rel: 'icon', url: '/trim.svg' }]
+	icons: [
+		{ rel: 'icon', type: 'image/svg+xml', url: '/trim.svg' },
+		{ rel: "icon", type: "image/png", sizes: "32x32", url: "/favicon-32x32.png" },
+		{ rel: "icon", type: "image/png", sizes: "16x16", url: "/favicon-16x16.png" },
+		{ rel: "apple-touch-icon", type: "image/png", sizes: "180x180", url: "/apple-touch-icon.png" },
+	],
+	manifest: '/manifest.webmanifest'
+}
+
+export const viewport: Viewport = {
+	themeColor: 'white',
 }
 
 export default function RootLayout({
